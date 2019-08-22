8829d9da30c200171b055ea46d93f7cdba878cea
drorfeige <53777256+drorfeige@users.noreply.github.com>
2019-08-22 08:55:38 +0300

    Add files via upload

---
+++ data_structers/voidarray/darray_generic.c
@@ -0,0 +1,198 @@
+#include "darray_generic.h"
+#include <stdlib.h>
+
+struct darray{ 
+	int m_initial_capacity;
+	int m_capacity;
+	int m_index;
+	void** m_Arr;
+
+};
+
+AdtStatus darrayCreate(darray** dArr, size_t initial_capacity){
+	darray* da;
+	da=(darray*)malloc(sizeof(darray));
+	if(da){
+		if((da->m_Arr=(void**)malloc(initial_capacity*sizeof(void*)))!=NULL){
+			da->m_capacity=initial_capacity;
+			da->m_initial_capacity=initial_capacity;
+			da->m_index=0;
+			*dArr=da;
+		}
+		else{
+			free(da);
+			return AllocationError;
+		}
+	} 
+	else{
+		return AllocationError;
+	}
+	return OK;
+}
+
+AdtStatus darrayDestroy(darray *dArr, elementDestroy destroyFunc, void *context){
+	int i=0;		
+	if(dArr!=NULL){
+		if(dArr->m_Arr!=NULL){
+			for(i=0;i<dArr->m_index;i++){
+				destroyFunc(dArr->m_Arr[i],context);
+			}
+			free(dArr->m_Arr);
+		}
+		free(dArr);
+		return OK;
+	}
+	return AllocationError;
+}
+
+AdtStatus   darrayAdd(darray *dArr,  void*  _item){
+	void* temp;
+	/**********************check pointers are not void*********************/
+	if(dArr==NULL || _item==NULL){
+		return NULL_POINTER;
+	}
+	if(dArr->m_Arr==NULL){
+		return NULL_POINTER;
+	}
+	/**************check if we need to realloc*************************/
+	if ((dArr->m_index)==(dArr->m_capacity)){
+		temp=dArr->m_Arr;
+		dArr->m_Arr=realloc(dArr->m_Arr,(dArr->m_capacity)*2*sizeof(void*));
+		if (dArr->m_Arr!=NULL){
+			dArr->m_capacity*=2;
+		}
+		else{
+			dArr->m_Arr=temp;
+			return AllocationError;
+		}
+	}
+	dArr->m_Arr[dArr->m_index++]=_item;
+	return OK;
+}
+
+AdtStatus   darrayGet(darray *dArr, size_t _index, void **_item){
+	/*check for errors*/
+	if(dArr==NULL || _item==NULL){
+		return NULL_POINTER;
+	}
+	if(dArr->m_Arr==NULL){
+		return NULL_POINTER;
+	}
+	if(_index>dArr->m_index){
+		return INVALID_INDEX;
+	}
+	/*get*/
+	*_item=dArr->m_Arr[_index];
+	return OK;
+}
+
+AdtStatus   darraySet(darray *dArr, size_t _index, void  *_item){
+
+		/*check for errors*/
+	if(dArr==NULL || _item){
+		return NULL_POINTER;
+	}
+	if(dArr->m_Arr==NULL){
+		return NULL_POINTER;
+	}
+	if(_index>dArr->m_index){
+		return INVALID_INDEX;
+	}
+	/*set*/
+	dArr->m_Arr[_index]=_item;
+	return OK;
+
+}
+
+AdtStatus   darrayDelete(darray *dArr,  void **_item){
+		void* temp;	
+	/*check for errors*/	
+	if(dArr==NULL || _item==NULL){
+		return NULL_POINTER;
+	}
+	if(dArr->m_Arr==NULL){
+		return NULL_POINTER;
+	}
+	/*check if we need to realloc*/
+	if(dArr->m_index<dArr->m_capacity*3/8 && dArr->m_capacity>=dArr->m_initial_capacity ){
+		temp=dArr->m_Arr;
+		dArr->m_Arr=realloc(dArr->m_Arr,dArr->m_capacity/2*sizeof(void*));
+		if (dArr->m_Arr!=NULL){
+			dArr->m_capacity/=2;
+		}
+		else{
+			dArr->m_Arr=temp;
+			return AllocationError;
+		}
+	}
+	*_item=dArr->m_Arr[dArr->m_index-1];
+	dArr->m_index--;
+	return OK;
+
+
+}
+
+AdtStatus   darrayItemsNum(darray *dArr, int*  _numOfItems){
+		/*check for errors*/
+	if(dArr==NULL || _numOfItems==NULL){
+		return NULL_POINTER;
+	}
+	if(dArr->m_Arr==NULL){
+		return NULL_POINTER;
+	}
+	/*return index*/
+	*_numOfItems=dArr->m_index;
+	return OK;
+
+}
+static void swap(void* x, void* y){
+	void* temp;
+	temp =x;
+	x=y;
+	y=temp;
+
+}
+static int partition (void** arr,int low, int high, elementCompare compareFunc){
+	int i=0;
+	int j=0;
+	void* pivot;  
+	pivot = arr[high];
+	i = (low - 1); 
+	for (j = low; j <= high- 1; j++){
+		if (compareFunc(arr[j],pivot)<=0){
+		i++;    
+		swap(arr[i],arr[j]);
+	        }
+	}
+	swap(arr[i+1], arr[high]);
+	return i+1;
+}
+static void quickSort(void** arr, int low,int high, elementCompare compareFunc){
+	int pi;
+	if (low < high){
+        pi = partition(arr, low, high,compareFunc);
+        quickSort(arr, low, pi - 1,compareFunc); 
+        quickSort(arr, pi + 1, high,compareFunc); 
+    }
+}
+
+AdtStatus darraySort(darray *dArr, elementCompare compareFunc){
+	
+	if(compareFunc==NULL || dArr==NULL){
+		return NULL_POINTER;
+	}
+	if(dArr->m_Arr==NULL){
+		return NULL_POINTER;
+	}
+	quickSort( dArr->m_Arr, 0, dArr->m_index-1, compareFunc);
+	return OK;
+}
+
+
+
+
+
+
+
+
+
---
+++ data_structers/voidarray/generictest.c
@@ -0,0 +1,156 @@
+#include "darray_generic.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int menu();
+struct points{
+	int x;
+	int y;
+};
+int addPoint(struct points** pt);
+int ptComp(void *PT1,void *PT2);
+void ptDestroy(void *PT,  void *FP);
+
+int main(int argc,char* argv[]){
+	int option=0; 
+	int ans=0;
+	int status=0;
+	darray* dA;
+	FILE* fp;
+	struct points* pt;
+ 	elementDestroy des;
+	elementCompare comp;
+	des=ptDestroy;
+	comp=ptComp;
+	fp=fopen("generic.txt","w");
+	if(fp==NULL){
+		return 0;
+	}
+	do{
+	printf("choose initial size of array:\n");
+	scanf("%d",&ans);
+	}while(ans<4);	
+	status=darrayCreate(&dA, ans);
+	if(status==AllocationError){
+		printf("allocation failed\n"); 
+		return 0;
+	}		
+	do{
+		option=menu();
+		switch (option){
+			case 1:
+				status=addPoint(&pt);
+				if(!ans){
+					printf("allocation failed\n"); 
+				}
+				status=darrayAdd(dA, (void*)pt);
+				if(status==AllocationError){
+					printf("allocation failed\n");
+					ptDestroy(pt,fp);
+					free(pt); 
+					return 0;
+				}
+				break;		
+			case 2:
+				status=darrayDelete(dA,(void**) &pt);
+				if(ans==OK){
+					ptDestroy(pt,fp);
+				}
+				break;
+			case 3:
+				printf("choose index to get\n");
+				scanf("%d",&ans);
+				status=darrayGet(dA, ans, (void**)&pt);
+				if(status==OK){
+					printf("the value of x is %d",pt->x);
+					printf("the value of y is %d\n",pt->y);
+				}
+				break;
+			case 4:
+				status=addPoint(&pt);
+				if(!status){
+					printf("allocation failed\n"); 
+				}
+				printf("choose index to set\n");
+				scanf("%d",&ans);
+				status=darraySet(dA, ans, (void*) pt);
+				break;
+			case 5:
+				status=darraySort(dA, comp); 
+				break;
+			case 6:
+				status=darrayItemsNum(dA, &ans);
+				printf("the number of items is %d.",ans);
+				break;
+			default:
+				break;
+		}
+	}while(option);
+	ans=darrayDestroy(dA, des, fp);
+	fclose(fp);
+	return 0;
+}
+
+int menu(){
+	int option=0;
+	do{
+	printf("choose option:\n");
+	printf("for exit choose 0\n");
+	printf("to add item choose 1\n");
+	printf("to delete item choose 2\n");
+	printf("to get item choose 3\n");
+	printf("to set item choose 4\n");
+	printf("to sort array choose 5\n");
+	printf("to count items choose 6\n");
+	scanf("%d",&option);
+	}while(option<0);
+	return option;
+}
+
+
+int addPoint(struct points** pt){
+	struct points* Pt;
+	int recieve;
+	Pt=(struct points*)malloc(sizeof(struct points));
+	if(Pt==NULL){
+		return 0;
+	}
+	printf("choose x coordinate:\n");
+	scanf("%d",&recieve);	
+	Pt->x=recieve;
+	printf("choose y coordinate:\n");
+	scanf("%d",&recieve);	
+	Pt->y=recieve;
+	*pt=Pt;
+	return 1;
+}
+
+int ptComp(void *PT1,void *PT2){
+	struct points *pt1;
+	struct points *pt2;
+	pt1 = (struct points*)PT2;
+	pt2 = (struct points*)PT2;
+	if(pt1==NULL || pt2==NULL){
+		return 0;
+	}
+	return (pt1->x)*(pt1->x)+(pt1->y)*(pt1->y)-(pt2->x)*(pt2->x)+(pt2->y)*(pt2->y);
+}
+
+void ptDestroy(void *PT,  void *FP){
+	struct points *pt;
+	FILE *fp;
+
+	pt = (struct points*)PT;
+	fp = (FILE*)FP;
+	if(pt==NULL || fp==NULL){
+		return;
+	}
+	if(pt){
+		fprintf(fp,"delted point: x=%d y=%d\n",pt->x,pt->y);
+	}
+	free(pt);
+}
+
+
+
+
