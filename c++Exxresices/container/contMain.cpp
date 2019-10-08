48dd10a4690da08e5b679dd2680dd094b9cc1aab
drorfeige <53777256+drorfeige@users.noreply.github.com>
2019-10-08 12:09:08 +0300

    Add files via upload

--- c++Exxresices/container/contMain.cpp
+++ c++Exxresices/container/contMain.cpp
@@ -1,10 +1,36 @@
 #include "tcontainer.h"
 #include <vector>
 #include <list>
+#include <iostream>
 
 using namespace std;
 
 int main(){
-	tContainer_t<int, vector<int*> > c1;
+	tContainer_t<int, list<int*> > c1;
+	cout << "is container empty: "<< c1.isEmpty() << "\n";
+	for (int i=0; i<7; ++i){
+		int* forCont= new int;
+		*forCont=i;
+		c1.insertEl(forCont);
+	}
+	cout << "is container empty: " << c1.isEmpty() << "\n";
+	cout << "the number of elements is : " << c1.numOfElements() << "\n";
+	cout << *c1[4] << "\n";
+	int* test;
+	test=(int*)c1.getFirst();
+	cout << "the first element is: " << *test << "\n";
+	test=(int*)c1.getLast();
+	cout << "the last element is: " << *test << "\n";
+	test=(int*)c1.getByVal(5);
+	cout << "is five? " << *test << "\n";
+	cout << "could delete 5? " << c1.deleteByVal(5) << "\n";
+	cout << "could delete 5? " << c1.deleteByVal(5) << "\n";
+	c1.deleteAll();
+	
 	return 0;
 }
+
+
+
+
+
--- c++Exxresices/container/tcontainer.h
+++ c++Exxresices/container/tcontainer.h
@@ -11,16 +11,17 @@ using namespace std;
 template <class T,class Container >
 class tContainer_t{
 	public:
-		virtual ~tContainer_t(){ removeAll();}
+		virtual ~tContainer_t(){ deleteAll();}
 		tContainer_t(){}	
 		inline bool isEmpty() const{return pCont.empty();}
 		inline size_t numOfElements() const{return pCont.size();}
-		inline void insertEl(T* newE){pCont.push_back(newE);}
-		const inline T* getFirst() const{return pCont.empty?0:pCont.front();}	
-		const inline T* getLast() const{return pCont.empty?0:pCont.back();}
+		inline void insertEl(const T* newE){pCont.push_back((T*)newE);}
+		const inline T* getFirst() const{return pCont.empty()?0:pCont.front();}	
+		const inline T* getLast() const{return pCont.empty()?0:pCont.back();}
 		const T* getByVal(const T& toFind) const{
 			c_iter_t it=find_if(pCont.begin(),pCont.end(),isEqual(toFind));
 			if(it==pCont.end()) return 0;
+			return *it;
 		}
 		T* removeByVal(const T& toFind){
 			iter_t it=find_if(pCont.begin(),pCont.end(),isEqual(toFind));
@@ -40,34 +41,39 @@ class tContainer_t{
 		void deleteAll(){
 			r_iter_t it;
 			while(!pCont.empty()){
-				it=pCont.rend();
+				it=pCont.rbegin();
 				delete *it;
 				pCont.pop_back();
 			}
 		}
 		const T* operator[](size_t index) const{
-				if(index>=pCont.size()) return 0;
-				if(typeid(Container)==typeid(vector<T*>)){
+				if(index>=pCont.size()) throw 2;
+				if(typeid(pCont)==typeid(vector<T*>)){
 					return pCont[index];
 				}	
-				else if(typeid(Container)==typeid(list<T*>)){
+				else if(typeid(pCont)==typeid(list<T*>)){
 					c_iter_t it=pCont.begin();
-					it++;
+					for(size_t i=0;i<index; i++){
+						it++;
+					}
 					return *it;
 				}
 				return 0;
 		}
 		T*& operator[](size_t index){
-				if(index>=pCont.size()) return 0;
-				if(typeid(Container)==typeid(vector<T*>)){
-					return pCont[index];
+				if(index>=pCont.size()) throw 2;
+				if(typeid(pCont)==typeid(vector<T*>)){
+					//return pCont[index];
+					return *pCont.begin(); // i get an error for the line above. i don't uderstand how rtti works
 				}	
-				else if(typeid(Container)==typeid(list<T*>)){
-					c_iter_t it=pCont.begin();
-					it++;
+				else if(typeid(pCont)==typeid(list<T*>)){
+					iter_t it=pCont.begin();
+					for(size_t i=0;i<index; i++){
+						it++;
+					}
 					return *it;
 				}
-				return 0;
+				throw 3;
 		}
 	private:
 		typedef typename Container::reverse_iterator r_iter_t; 
